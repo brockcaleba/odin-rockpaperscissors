a060fa930708e83ff5e6313562932604efb800fe
brockcaleba <113793370+brockcaleba@users.noreply.github.com>
2023-11-10 19:18:23 -0600

    Creat game rules and choice
    Put rules of game in a function called singleRound and created a prompt
    for user input to get their choice, also changed the input into
    lowercase

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,17 +10,50 @@ function getComputerChoice() {
 
 computerChoice = getComputerChoice();
 
-console.log(computerChoice);
+// console.log(computerChoice);
 
 // Number relates to either rock paper or scissors
 
-if (computerChoice === 1) {
-	computerPlay = 'Rock';
-} else if (computerChoice === 2) {
-	computerPlay = 'Paper';
-} else if (computerChoice === 3) {
-	computerPlay = 'Scissors';
+// if (computerChoice === 1) {
+// 	computerSelection = 'Rock';
+// } else if (computerChoice === 2) {
+// 	computerSelection = 'Paper';
+// } else if (computerChoice === 3) {
+// 	computerSelection = 'Scissors';
+// }
+
+// console.log(computerChoice);
+// console.log(computerSelection);
+
+// Create a function for a round of r-p-s
+function singleRound(playerSelection, computerSelection) {
+	if (playerSelection === computerSelection) {
+		return "It's a draw!";
+	} else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+		return 'Computer wins!';
+	} else if (
+		playerSelection === 'Paper' &&
+		computerSelection === 'Scissors'
+	) {
+		return 'Computer wins!';
+	} else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+		return 'Computer wins!';
+	} else if (playerSelection === '') {
+		return 'You did not enter anything';
+	} else {
+		return 'You win!';
+	}
 }
 
-console.log(computerChoice);
-console.log(computerPlay);
+// const playerSelection = 'Scissors';
+// const computerSelection = 'Rock';
+// console.log(singleRound(playerSelection, computerSelection));
+
+// Get users choice
+playerChoice = prompt('Enter your choice');
+
+// Change users choice to lowercase
+playerSelection = playerChoice.toLowerCase();
+
+// console.log(playerChoice);
+// console.log(playerSelection);
