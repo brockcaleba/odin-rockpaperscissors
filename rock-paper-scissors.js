57d6595dfe8be25f9a5e89c142218cfc51c28494
brockcaleba <113793370+brockcaleba@users.noreply.github.com>
2023-11-12 18:13:22 -0600

    Fix scoreboard
    Fix scoreboard so that it is functioning found but where computer
    chooses one and it sticks with that the whole five round game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -14,34 +14,41 @@ computerChoice = getComputerChoice();
 
 // Number relates to either rock paper or scissors
 
-if (computerChoice === 1) {
-	computerSelection = 'rock';
-} else if (computerChoice === 2) {
-	computerSelection = 'paper';
-} else if (computerChoice === 3) {
-	computerSelection = 'scissors';
+function getComputerSelection(computerChoice) {
+	if (computerChoice === 1) {
+		computerSelection = 'rock';
+	} else if (computerChoice === 2) {
+		computerSelection = 'paper';
+	} else if (computerChoice === 3) {
+		computerSelection = 'scissors';
+	}
 }
 
 // console.log(computerChoice);
 // console.log(computerSelection);
 
 // Create a function for a round of r-p-s
-function singleRound(playerSelection, computerSelection) {
+function singleRound(playerSelection, computerChoice) {
+	getComputerSelection(computerChoice);
 	let outputString = `You chose ${playerSelection} and the computer chose ${computerSelection}.`;
 	if (playerSelection === computerSelection) {
 		return `${outputString} It's a draw!`;
 	} else if (playerSelection === 'rock' && computerSelection === 'paper') {
+		computerScore++;
 		return `${outputString} Computer wins!`;
 	} else if (
 		playerSelection === 'paper' &&
 		computerSelection === 'scissors'
 	) {
+		computerScore++;
 		return `${outputString} Computer wins!`;
 	} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+		computerScore++;
 		return `${outputString} Computer wins!`;
 	} else if (playerSelection === '') {
 		return 'You did not enter anything';
 	} else {
+		playerScore++;
 		return `${outputString} You win!`;
 	}
 }
@@ -64,6 +71,8 @@ let computerScore = 0;
 let playerScore = 0;
 
 function round() {
+	getComputerChoice();
+	getComputerSelection(computerChoice);
 	playerChoice = prompt('Enter your choice');
 	playerSelection = playerChoice.toLowerCase();
 	if (
@@ -73,13 +82,30 @@ function round() {
 	) {
 		console.log('That is not an option');
 	} else {
-		console.log(singleRound(playerSelection, computerSelection));
+		console.log(singleRound(playerSelection, computerChoice));
 	}
 	console.log(`Computer Score: ${computerScore}`);
 	console.log(`Player Score: ${playerScore}`);
 }
 
 // Create function that plays 5 games in a row
-for (let i = 0; i < 5; i++) {
-	round();
+function game() {
+	for (let i = 0; i < 5; i++) {
+		round();
+	}
+	if (computerScore > playerScore) {
+		console.log(
+			`Computer: ${computerScore} | Player: ${playerScore} -- The computer wins`
+		);
+	} else if (playerScore > computerScore) {
+		console.log(
+			`Computer: ${computerScore} | Player: ${playerScore} -- The player wins`
+		);
+	} else if (playerScore === computerScore) {
+		console.log(
+			`Computer: ${computerScore} | Player: ${playerScore} -- It's a tie`
+		);
+	}
 }
+
+game();
